14e5cb2f743a22ddef648aeb4978bf30608bd493
DanielSola <daniel.sola.fraire@gmail.com>
2018-07-03 20:18:14 +0200

    Created PlottingService

--- main.py
+++ main.py
@@ -32,45 +32,61 @@ from numpy import pi
 
 class PlottingService():
     
-    def plot_demographic_data():
-        
-        demographic_data = DemographicData().get_demographic_data();
-        
-            #Age histogram
-    
-            age_hist, edges = np.histogram(demographic_data['age'], 
-                               bins = 17, 
-                               range = [0, 90])
-            ages = pd.DataFrame({'age': age_hist, 
-                       'left': edges[:-1], 
-                       'right': edges[1:]});
+    def __init__(self):       
+      self.demographic_data = demographic_data;
 
-            p1 = figure(plot_height = 600, plot_width = 600, 
-                       title = 'Histogram of Patient age at admission',
-                       x_axis_label = 'Age (years)', 
-                       y_axis_label = 'Number of patients');
-                        
-            p1.quad(bottom=0, top=ages['age'], 
-                    left=ages['left'], right=ages['right'], 
-                    fill_color='red', line_color='black');
+    def get_age_hist_figure(self):
+        
+        age_hist, edges = np.histogram(self.demographic_data['age'], 
+                           bins = 17, 
+                           range = [0, 90])
+        ages = pd.DataFrame({'age': age_hist, 
+                   'left': edges[:-1], 
+                   'right': edges[1:]});
+
+        p1 = figure(plot_height = 600, plot_width = 600, 
+                   title = 'Histogram of Patient age at admission',
+                   x_axis_label = 'Age (years)', 
+                   y_axis_label = 'Number of patients');
                     
-            #Gender pie chart
-            genders = ["Male","Female"];
-            gender_counts = [demographic_data.gender.value_counts()['M'], demographic_data.gender.value_counts()['F']];
-          
-            p2 = figure(x_range=(-1.2, 1.2), y_range=(-1.2, 1.2), title = 'Patient gender distribution');
-            p2.wedge(x=0 , y=0, radius=1, start_angle=-0.5*pi, end_angle=(gender_counts[0]/(gender_counts[0] + gender_counts[1]))*pi, color="blue", legend = 'Male: 32950 (56%)');
-            p2.wedge(x=0 , y=0, radius=1, start_angle=(gender_counts[0]/(gender_counts[0] + gender_counts[1]))*pi, end_angle=-0.5*pi, color="pink", legend = 'Female: 26026 (44%)');
+        p1.quad(bottom=0, top=ages['age'], 
+                left=ages['left'], right=ages['right'], 
+                fill_color='red', line_color='black');
+        
+        return p1;
+                
+    def get_gender_pie_figure(self):
+        
+        genders = ["Male","Female"];
+        gender_counts = [self.demographic_data.gender.value_counts()['M'], self.demographic_data.gender.value_counts()['F']];
+      
+        p2 = figure(x_range=(-1.2, 1.2), y_range=(-1.2, 1.2), title = 'Patient gender distribution');
+        p2.wedge(x=0 , y=0, radius=1, start_angle=-0.5*pi, end_angle=(gender_counts[0]/(gender_counts[0] + gender_counts[1]))*pi, color="blue", legend = 'Male: 32950 (56%)');
+        p2.wedge(x=0 , y=0, radius=1, start_angle=(gender_counts[0]/(gender_counts[0] + gender_counts[1]))*pi, end_angle=-0.5*pi, color="pink", legend = 'Female: 26026 (44%)');
+        
+        return p2;
+    
+    def get_marital_status_barplot(self):
+        
+        statuses = list(self.demographic_data.marital_status.value_counts().index);
+        counts = list(self.demographic_data.marital_status.value_counts());
+        source = ColumnDataSource(data=dict(statuses=statuses, counts=counts));
+        p3 = figure(x_range=statuses, plot_height=600, plot_width=600, toolbar_location=None, title="Marital status counts");
+        p3.vbar(x='statuses', top='counts', width=0.9, source=source, legend="statuses",
+                line_color='white', fill_color=factor_cmap('statuses', palette=Spectral6, factors=statuses));
+                          
+        return p3;
+    
+    def plot_figures(figures):
+        
+        grid = gridplot(figures);
 
-            #Marital status barplot
-            
-            statuses = list(demographic_data.marital_status.value_counts().index);
-            counts = list(demographic_data.marital_status.value_counts());
-            source = ColumnDataSource(data=dict(statuses=statuses, counts=counts));
-            p3 = figure(x_range=statuses, plot_height=600, plot_width=600, toolbar_location=None, title="Marital status counts");
-            p3.vbar(x='statuses', top='counts', width=0.9, source=source, legend="statuses",
-                    line_color='white', fill_color=factor_cmap('statuses', palette=Spectral6, factors=statuses));
-                              
-            grid = gridplot([[p1, p2, p3], [None, None]]);
+        show(grid);
+        
+PlottingService.demographic_data = demographic_data = DemographicData().get_demographic_data();
+figure1 = PlottingService().get_age_hist_figure();
+figure2 = PlottingService().get_gender_pie_figure();
+figure3 = PlottingService().get_marital_status_barplot();
 
-            show(grid);
+PlottingService.plot_figures([[figure1, figure2, figure3], [None, None]]);
+#PlottingService().plot_figures([[figure1, None, None], [None, None]]);
